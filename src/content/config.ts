e26d9a5ff330b0e4efece74d3e13c54e0525f7fe
tdriver <driverte@gmail.com>
2025-05-02 18:25:25 -0600

    Updating a couple of things

--- src/content/config.ts
+++ src/content/config.ts
@@ -92,17 +92,17 @@ const home = defineCollection({
     }),
 });
 
-const indexCards = defineCollection({
-  loader: glob({
-    pattern: "-index.{md,mdx}",
-    base: "./src/content/index-cards",
-  }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    cards: z.array(z.string()),
-  }),
-});
+// const indexCards = defineCollection({
+//   loader: glob({
+//     pattern: "-index.{md,mdx}",
+//     base: "./src/content/index-cards",
+//   }),
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     cards: z.array(z.string()),
+//   }),
+// });
 
 const poetry = defineCollection({
   loader: glob({ pattern: "**\/[^_]*.{md,mdx}", base: "./src/content/poetry" }),
@@ -169,7 +169,7 @@ export const collections = {
   blog,
   docs,
   home,
-  indexCards,
+  //indexCards,
   poetry,
   portfolio,
   recipes,
